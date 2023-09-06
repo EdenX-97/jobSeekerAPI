f0e458660d0866db432fabccb473c227ac4da9af
Mo Xu <edenx97@gmail.com>
2023-09-06 22:51:03 +1000

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,10 +9,20 @@ load_dotenv()
 
 # Create Flask app
 app = Flask(__name__)
-app.register_blueprint(jobTrackerApi, url_prefix='/api/jobTrackerApi')
 
-# Connect to database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
-db.init_app(app)
-with app.app_context():
-    db.create_all()
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+
+@app.route('/about')
+def about():
+    return 'About'
+# app.register_blueprint(jobTrackerApi, url_prefix='/api/jobTrackerApi')
+
+# # Connect to database
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
+# db.init_app(app)
+# with app.app_context():
+#     db.create_all()
